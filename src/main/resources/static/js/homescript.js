7f17491eb93cc45efc414422610e2ad7af402e16
phongnv307 <79550670+phongnv307@users.noreply.github.com>
2022-01-15 16:32:52 +0700

    update home

--- src/main/resources/static/js/homescript.js
+++ src/main/resources/static/js/homescript.js
@@ -1,10 +1,19 @@
-const serveMeal = document.querySelector('.serve-meal');
-serveMeal.addEventListener('click', function() {
-	serveMeal.style.display = "none";
-	var showEvent = document.querySelector('.show-event');
-	showEvent.style.display = "block";
-	var event = document.querySelector('.event');
-    event.style.backgroundImage = "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('image/bgevent.png')";
+// const serveMeal = document.querySelector('.serve-meal');
+// serveMeal.addEventListener('click', function() {
+// 	serveMeal.style.display = "none";
+// 	var showEvent = document.querySelector('.show-event');
+// 	showEvent.style.display = "block";
+// 	var event = document.querySelector('.event');
+//     event.style.backgroundImage = "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('image/bgevent.png')";
+// });
+
+const eventInfo = document.querySelector('.carousel-inner');
+eventInfo.addEventListener('click', function(event) {
+	var info = event.target.parentElement.children[1];
+	info.style.display = "block";
+	info.addEventListener('click', function(){
+		info.style.display = "none";
+	});
 });
 
 
